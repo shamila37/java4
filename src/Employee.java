a9bdc7bc1889c434db9c292d3f5ba1ad66dd1cb9
shamila37 <shamilaashan37@gmail.com>
2024-02-06 17:36:19 +0530

    Java sample code

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,26 @@
 public class Employee {
     int basic = 25000;
-
     int salary(){
         return basic;
+    }
+}
+class Manager extends Employee {
+    @Override
+    int salary() {
+        return basic + 50000;
+    }
+}
+class Executive extends Employee {
+    @Override
+    int salary() {
+        return basic + 30000;
+    }
+}
+class Test3 {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        System.out.println("Manager's salary " + manager.salary());
+        Executive executive = new Executive();
+        System.out.println("Executive's salary " + executive.salary());
     }
 }
